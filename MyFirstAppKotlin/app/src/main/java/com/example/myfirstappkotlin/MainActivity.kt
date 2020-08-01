148342129f0e477fe599fd09da5ec87d96f535f5
Jstyl8 <joseangel.jstyl8@gmail.com>
2020-08-01 13:42:41 +0200

    Add DisplayMessageActivity Trigger activity via intent from main activity Add activity to manifest

--- MyFirstAppKotlin/app/src/main/java/com/example/myfirstappkotlin/MainActivity.kt
+++ MyFirstAppKotlin/app/src/main/java/com/example/myfirstappkotlin/MainActivity.kt
@@ -1,8 +1,12 @@
 package com.example.myfirstappkotlin
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.EditText
+
+const val EXTRA_MESSAGE = "com.example.myfirstappkotlin.MESSAGE"
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,6 +16,11 @@ class MainActivity : AppCompatActivity() {
 
     /** Called when the user taps the Send button */
     fun sendMessage(view: View) {
-        // Do something in response to button
+        val editText = findViewById<EditText>(R.id.editText)
+        val message = editText.text.toString()
+        val intent = Intent(this, DisplayMessageActivity::class.java).apply {
+            putExtra(EXTRA_MESSAGE, message)
+        }
+        startActivity(intent)
     }
 }
